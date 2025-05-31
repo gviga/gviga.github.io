835c5c4dfc75d4eb9b5508a491e6c0451b3b8f86
gviga <95035641+gviga@users.noreply.github.com>
2025-05-31 08:25:52 +0000

    Deploying to gh-pages from @ gviga/gviga.github.io@5203226df4b3eb2f8c01de942809604ee98f8b36 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%67%69%75%6C%69%6F.%76%69%67%61%6E@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/gviga", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/giulio-viganò", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0002-0255-3240", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=ZN_WlJIAAAAJ", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -518,18 +546,11 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://sites.google.com/view/gvigano/home-page?authuser=0&pli=1", "_blank");
         },
       },{
       id: 'light-theme',
