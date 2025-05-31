48f08341ffcaff36a0c86c6ed7da52fe38df6d37
gviga <95035641+gviga@users.noreply.github.com>
2025-05-31 10:13:08 +0000

    Deploying to gh-pages from @ gviga/gviga.github.io@9078825bd8d514457be54289cebbe74866175e86 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
+  },{id: "nav-digital-garden",
+          title: "Digital Garden",
           description: "",
           section: "Navigation",
           handler: () => {
