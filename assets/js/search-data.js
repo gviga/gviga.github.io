1e1bb12c6b6bf9bc35faef7ee7a03c212df6d906
gviga <95035641+gviga@users.noreply.github.com>
2025-05-31 09:31:08 +0000

    Deploying to gh-pages from @ gviga/gviga.github.io@30c9a0bdec4cbf70f864496aefaa3eed96ceb12f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Teaching-related materials and experiences.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
