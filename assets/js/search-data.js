679a5e8cd6d1ffad9aa25340b2b6c3982096bc21
gviga <95035641+gviga@users.noreply.github.com>
2025-12-28 11:01:46 +0000

    Deploying to gh-pages from @ gviga/gviga.github.io@6345b11b4eaba8b159d221633c0d0a241357f15b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,7 +4,7 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-digital-garden",
-          title: "digital garden",
+        },{id: "nav-digitalgarden",
+          title: "DigitalGarden",
           description: "",
           section: "Navigation",
           handler: () => {
